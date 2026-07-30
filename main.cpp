0f72016a675fe2e69bce70d956bff8b2e47fefb8
DIVANSHU <sharmadivanshu999@gmail.com>
2026-07-30 22:40:53 +0530

    A code on input output and mathematical operation

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+// day 1 
+int main() {
+
+string name ; 
+cout << "Enter your name: ";
+cin >> name;
+
+cout << "my name is " << name << endl;
+
+int sum = 0;
+int num1 ;
+int num2 ;
+cout << "Enter two numbers: ";
+
+cin >> num1 >> num2;
+sum = num1 + num2;
+
+cout << " sum is " << sum << endl;
+
+
+
+
+
+    return 0;
+};
