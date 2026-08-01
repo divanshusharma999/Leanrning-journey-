8f3bcb0d717852a76bfa63e7bc6c2e3380abefba
DIVANSHU <sharmadivanshu999@gmail.com>
2026-08-01 14:30:14 +0530

    to write table of any input number n
    practice of loop

---
+++ table.cpp
@@ -0,0 +1,15 @@
+// multiplication table 
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Multiplication table for " << num << ":" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+
+    return 0;
+}
